6d8418a857d1e80c605a48ecfa972498dee9707c
Henrique Rossi <51084793+henriqrossi@users.noreply.github.com>
2019-06-05 11:24:23 -0300

    atualização

---
+++ primeiroprojeto/logado.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+	<meta charset="utf-8">
+	<title>Logado, parabéns!</title>
+	<style type="text/css">
+		#joia {
+			width: 400px;
+			height: 300px;
+			margin: 100px;
+		}
+		a{
+			font-size: 20pt;
+			margin: 650px;
+		}
+	</style>
+</head>
+<body>
+	<div align="center">
+		<img id="joia" src="assets/img/curtas0329-08.jpg" alt="curtas0329-08"/>
+	</div>
+		<a href="index.html">Voltar</a>
+
+	<script type="text/javascript">
+		alert ("Olá! Seja bem-vindo!")
+	</script>
+</body>
+</html>
